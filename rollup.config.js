5420609d9bb9d4a4742bd8019da828d674bef712
liaozhai <marunov@scanex.ru>
2019-07-01 12:21:43 +0300

    Update

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import cpy from 'rollup-plugin-cpy';
+import copy from 'rollup-plugin-copy';
 import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
@@ -18,9 +18,11 @@ export default {
         resolve(),            
         commonjs(),
         json(),
-        cpy([
-            {files: 'src/*.png', dest: './dist'}
-        ]),
+        copy({
+            targets: [
+                {src: 'src/*.png', dest: './dist'}
+            ]
+        }),
         babel({
             include: 'node_modules/svelte/shared.js',
             exclude: 'node_modules/**'
